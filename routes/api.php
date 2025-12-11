286fb2510104c8cae5453e6c83b8dfb1e26d9cca
Thakvika1 <thakvika.thorn@gmail.com>
2025-12-12 03:24:19 +0700

    note in route

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,12 @@ use App\Http\Controllers\Api\Authentication\LogoutController;
 // })->middleware('auth:sanctum');
 
 
+// Public Routes
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 
 
+// Protected Routes
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [LogoutController::class, 'logout']);
 });
