46c17f0b60151d22a34b4a0a871a25fb2c96d86b
Thakvika1 <thakvika.thorn@gmail.com>
2025-12-22 17:42:44 +0700

    scale cart controller

---
+++ app/Http/Controllers/Api/User/Cart/UpdateCartController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\User\Cart;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdateCartController extends Controller
+{
+    //
+}
