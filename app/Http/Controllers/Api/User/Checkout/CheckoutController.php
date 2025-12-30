1efa6d0328d39cfdc21228b5d34ebd763b6efb08
Thakvika1 <thakvika.thorn@gmail.com>
2025-12-30 18:05:25 +0700

    refactor

--- app/Http/Controllers/Api/User/Checkout/CheckoutController.php
+++ app/Http/Controllers/Api/User/Checkout/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\User\Checkout;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\CheckoutService;
+use Exception;
 
 class CheckoutController extends Controller
 {
@@ -12,10 +13,16 @@ class CheckoutController extends Controller
     {
         $order = $service->checkout();
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Order successfully',
-            'order' => $order->load('orderItems')
-        ]);
+        try {
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Order successfully',
+                'order' => $order->load('orderItems')
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 }
